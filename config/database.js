a675d9e4190a3975d559cdd8b9591fd8329fdcd8
Lokesh <lokesh@superdev.cloud>
2023-04-27 17:18:28 +0530

    Initial setup

---
+++ config/database.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_DATABASE'),
+      user: env('DATABASE_USER'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool(true),
+    },
+    pool: { min: 0, max: 10 }
+  },
+});
